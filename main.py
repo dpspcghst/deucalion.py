53b51f3e75c67fe54cc13618c1b2e7bebae11a6d
deepspaceghost <deepspaceghost@protonmail.com>
2022-06-17 21:18:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import logging
 from command_center import CommandCenter as cc
+
+# sets up err.log for file handling
+handler = logging.FileHandler(
+    encoding="utf-8",
+    filename="deucalion_err.log",
+    mode="w"
+)
+
+# determines how the log will be formatted
+handler.setFormatter(logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s"))
+
+logger = logging.getLogger("file manager")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 class Deucalion():
     """
